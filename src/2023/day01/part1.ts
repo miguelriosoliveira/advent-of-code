c629e1ef30ddcbe64c18684e07f05c234061432a
Miguel Rios <miguelriosoliveira@gmail.com>
2023-12-04 20:12:20 +0100

    lint: Autofix

--- src/2023/day01/part1.ts
+++ src/2023/day01/part1.ts
@@ -1,26 +1,26 @@
-import fs from "fs";
-import readline from "readline";
+import fs from 'fs';
+import readline from 'readline';
 
 function isNumeric(c: string): boolean {
-  return !Number.isNaN(Number(c));
+	return !Number.isNaN(Number(c));
 }
 
 export async function part1(filePath: string): Promise<number> {
-  const rl = readline.createInterface({
-    input: fs.createReadStream(filePath),
-    crlfDelay: Number.POSITIVE_INFINITY,
-  });
+	const rl = readline.createInterface({
+		input: fs.createReadStream(filePath),
+		crlfDelay: Number.POSITIVE_INFINITY,
+	});
 
-  let calibrationSum = 0;
+	let calibrationSum = 0;
 
-  for await (const line of rl) {
-    const chars = line.split("");
+	for await (const line of rl) {
+		const chars = line.split('');
 
-    const firstNumber = chars.find((c) => isNumeric(c))!;
-    const secondNumber = chars.findLast((c) => isNumeric(c))!;
+		const firstNumber = chars.find(c => isNumeric(c))!;
+		const secondNumber = chars.findLast(c => isNumeric(c))!;
 
-    calibrationSum += Number(firstNumber + secondNumber);
-  }
+		calibrationSum += Number(firstNumber + secondNumber);
+	}
 
-  return calibrationSum;
+	return calibrationSum;
 }
--- src/2023/day01/part2.ts
+++ src/2023/day01/part2.ts
@@ -1,47 +1,47 @@
-import fs from "fs";
-import readline from "readline";
+import fs from 'fs';
+import readline from 'readline';
 
 type Match = [group: string, match: keyof typeof NUMBERS];
 
 const NUMBERS = {
-  "1": 1,
-  one: 1,
-  "2": 2,
-  two: 2,
-  "3": 3,
-  three: 3,
-  "4": 4,
-  four: 4,
-  "5": 5,
-  five: 5,
-  "6": 6,
-  six: 6,
-  "7": 7,
-  seven: 7,
-  "8": 8,
-  eight: 8,
-  "9": 9,
-  nine: 9,
+	'1': 1,
+	one: 1,
+	'2': 2,
+	two: 2,
+	'3': 3,
+	three: 3,
+	'4': 4,
+	four: 4,
+	'5': 5,
+	five: 5,
+	'6': 6,
+	six: 6,
+	'7': 7,
+	seven: 7,
+	'8': 8,
+	eight: 8,
+	'9': 9,
+	nine: 9,
 } as const;
 
-const NUMBER_REGEX = new RegExp(`(?=(${Object.keys(NUMBERS).join("|")}))`, "g");
+const NUMBER_REGEX = new RegExp(`(?=(${Object.keys(NUMBERS).join('|')}))`, 'g');
 
 export async function part2(filePath: string): Promise<number> {
-  const rl = readline.createInterface({
-    input: fs.createReadStream(filePath),
-    crlfDelay: Number.POSITIVE_INFINITY,
-  });
+	const rl = readline.createInterface({
+		input: fs.createReadStream(filePath),
+		crlfDelay: Number.POSITIVE_INFINITY,
+	});
 
-  let calibrationSum = 0;
+	let calibrationSum = 0;
 
-  for await (const line of rl) {
-    const match = Array.from(line.matchAll(NUMBER_REGEX));
+	for await (const line of rl) {
+		const match = Array.from(line.matchAll(NUMBER_REGEX));
 
-    const [, firstNumberKey] = match.at(0) as Match;
-    const [, secondNumberKey] = match.at(-1) as Match;
+		const [, firstNumberKey] = match.at(0) as Match;
+		const [, secondNumberKey] = match.at(-1) as Match;
 
-    calibrationSum += NUMBERS[firstNumberKey] * 10 + NUMBERS[secondNumberKey];
-  }
+		calibrationSum += NUMBERS[firstNumberKey] * 10 + NUMBERS[secondNumberKey];
+	}
 
-  return calibrationSum;
+	return calibrationSum;
 }
