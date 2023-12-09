11c0c6ad6d3782fc20c415eb7ce377a6c094cbab
Miguel Rios <miguelriosoliveira@gmail.com>
2023-12-09 22:12:45 +0100

    Day 4: part 2 (README)

--- src/2023/day04/part1.test.ts
+++ src/2023/day04/part1.test.ts
@@ -3,7 +3,7 @@ import { part1 } from './part1';
 
 const inputsDir = path.resolve(__dirname, './inputs/');
 
-describe('#day03 - part one', () => {
+describe('#day04 - part one', () => {
 	it('should resolve to 13', async () => {
 		const filePath = path.resolve(inputsDir, './example-input-1.txt');
 		await expect(part1(filePath)).resolves.toBe(13);
